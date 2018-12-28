435a38fdbb7111a1b9a160aab26d9998c357a1c7
azul <azulkipli@gmail.com>
2018-12-28 16:24:47 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import {
+  Toolbar,
+  ToolbarRow,
+  ToolbarSection,
+  ToolbarTitle,
+  ToolbarMenuIcon,
+  ToolbarIcon
+} from "@rmwc/toolbar";
+
+import { TabBar, Tab } from "@rmwc/tabs";
+
+import {
+  GridList,
+  GridTile,
+  GridTileIcon,
+  GridTilePrimary,
+  GridTilePrimaryContent,
+  GridTileSecondary,
+  GridTileTitle
+} from "@rmwc/grid-list";
+
+import { Checkbox } from "@rmwc/checkbox";
+import { Select } from "@rmwc/select";
+
+import {
+  Drawer,
+  DrawerHeader,
+  DrawerContent,
+  DrawerTitle,
+  DrawerSubtitle
+} from "@rmwc/drawer";
+
+import { List, ListItem } from "@rmwc/list";
+
+import "@material/toolbar/dist/mdc.toolbar.min.css";
+import "@material/top-app-bar/dist/mdc.top-app-bar.min.css";
+import "@material/grid-list/dist/mdc.grid-list.css";
+import "@material/select/dist/mdc.select.min.css";
+import "@material/checkbox/dist/mdc.checkbox.min.css";
+import "@material/drawer/dist/mdc.drawer.min.css";
+
+import "@material/tab-bar/dist/mdc.tab-bar.css";
+import "@material/tab/dist/mdc.tab.css";
+import "@material/tab-scroller/dist/mdc.tab-scroller.css";
+import "@material/tab-indicator/dist/mdc.tab-indicator.css";
 
 class App extends Component {
+  state = {
+    tileGutter1: false,
+    headerCaption: false,
+    twolineCaption: false,
+    withIconAlignStart: false,
+    modalOpen: false,
+    activeTab: 0
+  };
   render() {
+    const { modalOpen, activeTab } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Toolbar>
+          <ToolbarRow>
+            <ToolbarSection alignStart>
+              <ToolbarMenuIcon
+                icon="menu"
+                onClick={() => this.setState({ modalOpen: !modalOpen })}
+              />
+              <ToolbarTitle>Toolbar</ToolbarTitle>
+            </ToolbarSection>
+            <ToolbarSection alignEnd>
+              <ToolbarIcon icon="save" />
+              <ToolbarIcon icon="print" />
+            </ToolbarSection>
+          </ToolbarRow>
+        </Toolbar>
+
+        <Drawer modal open={modalOpen} onClose={() => this.setState({ modalOpen: false })}>
+          <DrawerHeader>
+            <DrawerTitle>DrawerHeader</DrawerTitle>
+            <DrawerSubtitle>Subtitle</DrawerSubtitle>
+          </DrawerHeader>
+          <DrawerContent>
+            <List>
+              <ListItem>Cookies</ListItem>
+              <ListItem>Pizza</ListItem>
+              <ListItem>Icecream</ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+
+        <section className="content">
+          <GridList
+            tileGutter1={this.state.tileGutter1}
+            headerCaption={this.state.headerCaption}
+            twolineCaption={this.state.twolineCaption}
+            withIconAlignStart={this.state.withIconAlignStart}
+            tileAspect={this.state.tileAspect}>
+            {[...Array(4)].map((val, i) => (
+              <GridTile key={i}>
+                <GridTilePrimary>
+                  <GridTilePrimaryContent>
+                    <img
+                      src="https://material-components-web.appspot.com/images/1-1.jpg"
+                      alt="test"
+                    />
+                  </GridTilePrimaryContent>
+                </GridTilePrimary>
+                <GridTileSecondary>
+                  <GridTileIcon icon="info" />
+                  <GridTileTitle>Tile {i + 1}</GridTileTitle>
+                </GridTileSecondary>
+              </GridTile>
+            ))}
+          </GridList>
+
+          <Checkbox
+            label="tileGutter1"
+            onClick={() => this.setState({ tileGutter1: !this.state.tileGutter1 })}
+          />
+          <Checkbox
+            label="headerCaption"
+            onClick={() => this.setState({ headerCaption: !this.state.headerCaption })}
+          />
+          <Checkbox
+            label="twolineCaption"
+            onClick={() => this.setState({ twolineCaption: !this.state.twolineCaption })}
+          />
+          <Checkbox
+            label="withIconAlignStart"
+            onClick={() => this.setState({ withIconAlignStart: !this.state.withIconAlignStart })}
+          />
+
+          <Select
+            value={this.state.tileAspect || "1x1"}
+            onChange={evt => this.setState({ tileAspect: evt.target.value })}
+            label="tileAspect"
+            options={["1x1", "16x9", "2x3", "3x2", "4x3", "3x4"]}
+          />
+        </section>
+        <section className="content">
+          {/* Controlled */}
+          <TabBar
+            activeTabIndex={activeTab}
+            onActivate={evt => this.setState({activeTab: evt.detail.index})}
           >
-            Learn React
-          </a>
-        </header>
+            <Tab>Cookies</Tab>
+            <Tab>Pizza</Tab>
+            <Tab>Icecream</Tab>
+          </TabBar>
+        </section>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// import 'material-components-web/dist/material-components-web.min.css';
+import { RMWCProvider } from '@rmwc/provider';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RMWCProvider><App /></RMWCProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
